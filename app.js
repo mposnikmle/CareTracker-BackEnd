aab5b579196e5003bd2d73aaff321cdc17de6f89
Dominic Gangi <76754852+djg9@users.noreply.github.com>
2023-11-09 15:41:02 -0500

    Added staff controller

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const staffController = require("./controllers/staff.controller");
+
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT;
+const DBName = process.env.DBNAME;
+const DB_URL = process.env.DB_URL;
+
+mongoose.connect(DB_URL + DBName);
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("connected to the DB", DBName);
+});
 
 app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`);
 });
+
+app.use(express.json());
+
+app.use("/staff", staffController);
---
+++ middleware/validate-session.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const Staff = require("../models/staff.model");
+
+const validateSession = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await Staff.findById(decodedToken.id);
+
+        if (!staff) {
+            throw new Error("Staff member not found");
+        }
+
+        req.staff = staff;
+
+        return next();
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+module.exports = validateSession;
